98df8452d7fed5dea674c109a895163a564c87c8
shyam04616 <shyamsingh04616@gmail.com>
2024-12-13 14:03:53 +0530

    Till React Router

---
+++ 07reactRouter/src/layout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+import { Outlet } from "react-router-dom";
+import Contact from "./components/Contact/Contact";
+function Layout(){
+    return(
+        <>
+        <Header/>
+        <Outlet/>
+        <Contact/>
+        <Footer/>
+        </>
+    )
+}
+
+export default Layout
